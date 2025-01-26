debbae88ee69c31c75607e5bc821e22c17472feb
Marlon Villalona <iAmVillalona@gmail.com>
2025-01-26 12:30:01 -0400

    fix/ remove error and add HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,28 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using vipeticket.Models;
+using Vipe_Tickets.Models;
 
-namespace vipeticket.Controllers
+namespace YourNamespace.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
