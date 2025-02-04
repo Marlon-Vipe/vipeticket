a67f65ad28dbf0962cb9d7b8e3f16448df93e9e0
Marlon Villalona <iAmVillalona@gmail.com>
2025-02-04 11:20:16 -0400

    feat/ add new Ticket History Controller

---
+++ Controllers/TicketHistoriesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Vipe_Tickets.Models;
+
+namespace Vipe_Tickets.Controllers
+{
+    public class TicketHistoriesController : Controller
+    {
+        private readonly VipeTicketsContext _context;
+
+        public TicketHistoriesController(VipeTicketsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TicketHistories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TicketHistories.ToListAsync());
+        }
+
+        // GET: TicketHistories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistories
+                .FirstOrDefaultAsync(m => m.HistoryId == id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TicketHistories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HistoryId,TicketId,StatusId,ChangedBy,ChangedAt,Notes")] TicketHistory ticketHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ticketHistory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistories.FindAsync(id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+            return View(ticketHistory);
+        }
+
+        // POST: TicketHistories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HistoryId,TicketId,StatusId,ChangedBy,ChangedAt,Notes")] TicketHistory ticketHistory)
+        {
+            if (id != ticketHistory.HistoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ticketHistory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketHistoryExists(ticketHistory.HistoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistories
+                .FirstOrDefaultAsync(m => m.HistoryId == id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketHistory);
+        }
+
+        // POST: TicketHistories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ticketHistory = await _context.TicketHistories.FindAsync(id);
+            if (ticketHistory != null)
+            {
+                _context.TicketHistories.Remove(ticketHistory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketHistoryExists(int id)
+        {
+            return _context.TicketHistories.Any(e => e.HistoryId == id);
+        }
+    }
+}
